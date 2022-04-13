abc243096ef79d30fd1b49edfbea43e02a5ae485
zozo222 <doerrzoran@gmail.com>
2022-04-13 10:33:49 +0200

    mise à jour du traitement des reservations

--- Back/ConnectToDatabase.php
+++ Back/ConnectToDatabase.php
@@ -22,6 +22,13 @@ function selectFromDatabase($table, $key, $data, $conn){
   return $row;
 }
 
+function selectAndFromDatabase($table, $key1, $data1, $key2, $data2, $conn){
+  $sql = "SELECT * FROM $table WHERE $key1 = '$data1' AND $key2 = '$data2'";
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  return $row;
+}
+
 function getEstablishmentAndSuite($conn){
   $sql = "SELECT e.ID, e.Name, s.ID suite_id, s.Title FROM establishment e JOIN suite s ON s.establishment = e.ID";
   $result = mysqli_query($conn, $sql);
--- Back/CreateSuiteForm.php
+++ Back/CreateSuiteForm.php
@@ -4,12 +4,17 @@ $establishmentID = (int) ($_POST['data']);
 
 require "ConnectToDatabase.php";
 
-$query = selectFromDatabase('suite', 'Establishment', $establishmentID, $conn);
+$query = selectAndFromDatabase('suite', 'Establishment', $establishmentID, 'isOccupied', 0, $conn);
+// $sql = "SELECT * FROM suite WHERE Establishment = '$establishmentID' AND isOccupied = 0";
+// $result = mysqli_query($conn, $sql);
+// $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
 $suiteArr = [];
 
 foreach($query as $row) {
-    $suiteArr[$row['ID']] = $row['Title'];
+    $suiteArr[$row['ID']] = $row['Title']; 
 }
 
-print_r(json_encode($suiteArr));
+  print_r(json_encode($suiteArr));
 
