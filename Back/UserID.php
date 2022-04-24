b063a82194a4fbfbf7ee25292773cb418eb907c7
zozo222 <doerrzoran@gmail.com>
2022-04-25 01:14:32 +0200

    test

--- Back/UserID.php
+++ Back/UserID.php
@@ -3,14 +3,13 @@
 
 session_start();
 if(isset($_SESSION['id'])){
+    echo 'oui';
     $user = $_SESSION['id'];
-    echo $user;
-    $name = $_SESSION['nme'];
+    $name = $_SESSION['name'];
     $firstname = $_SESSION['firstname'];
     $mail = $_SESSION['mail'];
     $password = $_SESSION['password'];
     $role = $_SESSION['role'];
-    echo $role;
 }else{
 
 }
