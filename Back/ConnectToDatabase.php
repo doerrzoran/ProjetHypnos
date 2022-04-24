5237ed488a99092adc3ba324065ddffdcc5e1d9a
zozo222 <doerrzoran@gmail.com>
2022-04-25 00:17:02 +0200

    test

--- Back/ConnectToDatabase.php
+++ Back/ConnectToDatabase.php
@@ -15,7 +15,7 @@ function selectFromDatabase($table, $key, $data, $conn){
   // $inter = $sql;
   // echo($sql.'/n');
   // $result = pg_query($conn, $inter);
-  $result = pg_query($conn, "select $key from user_hypnos");
+  $result = pg_query($conn, "select $key from $table");
   $row = pg_fetch_row($result);
   return $row;
 }
