696ca10a2322194674fbbfc3146113bf9687b319
zozo222 <doerrzoran@gmail.com>
2022-04-24 22:30:11 +0200

    test

--- Front/temp.php
+++ Front/temp.php
@@ -2,5 +2,5 @@
     require_once "../Back/ConnectToDatabase.php";
     selectFromDatabase("user_hypnos", "name", "Doerr", $conn);
     echo("Resultat ici :".'</br>');
-    print_r($result);
+    print_r($row);
 ?>
