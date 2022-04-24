1449332762a6459821e07cd87ddfc8c63516d5de
zozo222 <doerrzoran@gmail.com>
2022-04-24 23:27:47 +0200

    test

--- Front/temp.php
+++ Front/temp.php
@@ -2,12 +2,12 @@
     require_once "../Back/ConnectToDatabase.php";
     // $resultquery = selectFromDatabase("user_hypnos", "name", "Doerr", $conn);
     $resultquery = selectFromDatabase("select * from user_hypnos", $conn);
-    echo("Resultat ici :".'</br>');
-    if(!$resultquery)
-    {
-        echo("pas de résultats");
-    }
-    else {
-        print_r($resultquery);
-    }
+    // echo("Resultat ici :".'</br>');
+    // if(!$resultquery)
+    // {
+    //     echo("pas de résultats");
+    // }
+    // else {
+    //     print_r($resultquery);
+    // }
 ?>
