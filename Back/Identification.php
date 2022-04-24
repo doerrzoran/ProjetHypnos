3a58150cabc639f2637acf5d4db39162a8486415
zozo222 <doerrzoran@gmail.com>
2022-04-25 00:41:48 +0200

    test

--- Back/Identification.php
+++ Back/Identification.php
@@ -6,8 +6,9 @@ $users = selectFromDatabase('user_hypnos', 'Mail', $mail, $conn);
 if($users == NULL){
   echo "utilisateur inconnu!"; 
 }else{
-  
+  echo 'utilisateur identifié</br>';
   foreach($users as $user){
+    echo 'toujours</br>';
     session_start();
     $_SESSION['ID'] = $user['ID'];
     $_SESSION['Name'] = $user['Name'];
@@ -15,6 +16,7 @@ if($users == NULL){
     $_SESSION['Mail'] = $user['Mail'];
     $_SESSION['Password'] = $user['Password'];
     $_SESSION['Role'] = $user['Role'];
+    echo 'encore</br>';
     require_once 'UserRedirection.php';
   }
 
