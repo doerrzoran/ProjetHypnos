68fa4ec7a26645deb2ea838c8862dac800cd8478
zozo222 <doerrzoran@gmail.com>
2022-04-24 13:00:24 +0200

    test

--- Back/formReservation.php
+++ Back/formReservation.php
@@ -22,7 +22,7 @@ if(isset($_SESSION['Suite'])){
 }else{
   
 
-  require_once ($_SERVER['DOCUMENT_ROOT']."Back/ConnectToDatabase.php");
+  require_once "../Back/ConnectToDatabase.php";
  
   $result = getEstablishmentAndSuite($conn);
 
