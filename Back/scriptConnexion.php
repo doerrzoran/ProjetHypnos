8e7b75337659343b2aa9c9bfc0bdda9b092d7b2a
zozo222 <doerrzoran@gmail.com>
2022-04-24 16:37:02 +0200

    test

--- Back/scriptConnexion.php
+++ Back/scriptConnexion.php
@@ -2,6 +2,7 @@
 require_once "ConnectToDatabase.php";
 
 $infoConnexion = $_POST;
+var_dump($infoConnexion);
 
 If(!isset($infoConnexion['Mail']) || !isset($infoConnexion['password'])){
   echo ' ';
