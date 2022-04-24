133b137166677d703d17c78d497ec181fb71c026
zozo222 <doerrzoran@gmail.com>
2022-04-25 01:31:31 +0200

    test

--- Back/Identification.php
+++ Back/Identification.php
@@ -8,12 +8,12 @@ if($user == NULL){
   echo $user['0'];
     session_start();
    var_dump($user['0']);
-    $_SESSION['0'] = $user['id'];
-    $_SESSION['1'] = $user['name'];
-    $_SESSION['2'] = $user['firstname'];
-    $_SESSION['3'] = $user['mail'];
-    $_SESSION['4'] = $user['password'];
-    $_SESSION['5'] = $user['role'];
+    $_SESSION['id'] = $user['0'];
+    $_SESSION['name'] = $user['1'];
+    $_SESSION['firstname'] = $user['2'];
+    $_SESSION['mail'] = $user['3'];
+    $_SESSION['password'] = $user['4'];
+    $_SESSION['role'] = $user['5'];
     var_dump($_SESSION['id']).'</br>';
     require_once 'UserRedirection.php';
 }
