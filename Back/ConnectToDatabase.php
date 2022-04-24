559b6e513d4a81721bcb0360a34e3fd7fb1c4e25
zozo222 <doerrzoran@gmail.com>
2022-04-24 13:32:18 +0200

    test

--- Back/ConnectToDatabase.php
+++ Back/ConnectToDatabase.php
@@ -1,51 +1,51 @@
 <?php
 echo 'hello';
 // connect to database
-$conn = mysqli_connect('localhost', 'Zoran', 'test1234', 'hypnos_bdd');
+$conn = pg_connect('postgresql-tapered-22363');
 
 // check connection
 
 if(!$conn){
-  die('Connection error: '). mysqli_connect_error();
+  die('Connection error: '). pg_connect_error();
 }
 
 
 function insertintoDatabase($table, $key, $data, $conn){
   $sql = "INSERT INTO $table($key) VALUES($data)";
-  $result = mysqli_query($conn, $sql);
+  $result = pg_query($conn, $sql);
   return $result;
 }
 
 function selectFromDatabase($table, $key, $data, $conn){
   $sql = "SELECT * FROM $table WHERE $key = '$data'";
-  $result = mysqli_query($conn, $sql);
-  $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  $result = pg_query($conn, $sql);
+  $row = pg_fetch_all($result, MYSQLI_ASSOC);
   return $row;
 }
 
 function selectAndFromDatabase($table, $key1, $data1, $key2, $data2, $conn){
   $sql = "SELECT * FROM $table WHERE $key1 = '$data1' AND $key2 = '$data2'";
-  $result = mysqli_query($conn, $sql);
-  $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  $result = pg_query($conn, $sql);
+  $row = pg_fetch_all($result, MYSQLI_ASSOC);
   return $row;
 }
 
 function getEstablishmentAndSuite($conn){
   $sql = "SELECT e.ID, e.Name, s.ID suite_id, s.Title FROM establishment e JOIN suite s ON s.establishment = e.ID";
-  $result = mysqli_query($conn, $sql);
-  $row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  $result = pg_query($conn, $sql);
+  $row = pg_fetch_all($result, MYSQLI_ASSOC);
   return $row;
 }
 
 function updateDatabase($table, $line, $newvalue, $key, $data, $conn){
   $sql = "UPDATE $table SET $line = '$newvalue' WHERE $key = '$data'";
-  $result = mysqli_query($conn, $sql);
+  $result = pg_query($conn, $sql);
   return $result;
 }
 
 
 function deleteFromDatabase($table, $key, $data, $conn){
   $sql = "DELETE FROM $table WHERE $key = '$data'";
-  $result = mysqli_query($conn, $sql);
+  $result = pg_query($conn, $sql);
   return $result;
 }
