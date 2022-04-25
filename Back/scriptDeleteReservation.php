8612f47e90d882364b1e876e572fb04d2d5327cb
zozo222 <doerrzoran@gmail.com>
2022-04-25 18:24:27 +0200

    test

--- Back/scriptDeleteReservation.php
+++ Back/scriptDeleteReservation.php
@@ -9,7 +9,7 @@ if(isset($infoDeletion)){
 
     $deletion = deleteFromDatabase('booking', 'id', $bookingID, $conn);
     var_dump($deletion);
-    if($deletion == 1){
+    if($deletion != 1){
        header('location: ../Front/PageClient.php');
     }
 }else{
