310c39b7be4c7e770942773f2ed132f94ff42430
zozo222 <doerrzoran@gmail.com>
2022-04-25 10:03:44 +0200

    test

--- Front/temp.php
+++ Front/temp.php
@@ -3,7 +3,8 @@
     require_once "../Back/ConnectToDatabase.php";
     echo 'test</br>';
     // $query = pg_insert($conn, 'user_hypnos', ['test', 'testant', 'test@mail.com', 'test', 3]);
-    $query = "INSERT INTO user_hypnos (id, name, firstnam, mail, role, password) VALUES (153145315151, test, testant, test@mail.com, test, 3)";
+    $query = "INSERT INTO user_hypnos (id, name, firstname, mail, role, password) VALUES (153145315151, test, testant, test@mail.com, test, 3)";
+    // $query = "INSERT INTO users (email, name, surname) VALUES ('$email', '$name', '$surname')"
     // "INSERT INTO book VALUES ('$name','$firstname', '$email','$role')";
     if (pg_query($conn,$query))
 {
