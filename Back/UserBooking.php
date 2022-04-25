4bb8723327956610367499863caf4e6cd51c3eeb
zozo222 <doerrzoran@gmail.com>
2022-04-25 18:33:03 +0200

    test

--- Back/UserBooking.php
+++ Back/UserBooking.php
@@ -1,12 +1,12 @@
 <?php
  require_once 'BookingData.php';
-     
+    if(isset($booking)){ 
         $bookingID = $booking['0'];
         $bookingSD = $booking['1'];
         $bookingED = $booking['2'];
         $bookingSuite = $booking['3'];
         $booking = $booking['4'];
-        echo 'votre reservations: du '.$bookingSD.' au '.$bookingED.'</br>';
+        echo 'votre reservation: du '.$bookingSD.' au '.$bookingED.'</br>';
 
         $origin = new DateTime($bookingSD);
         $target = new DateTime(date('Y-m-d'));
@@ -17,4 +17,4 @@
         }else{
                  
         }    
-  
+}   
