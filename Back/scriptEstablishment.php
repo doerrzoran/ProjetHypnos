a0935e5acdda87bacbc108fd9b7f1bf3af9fb462
zozo222 <doerrzoran@gmail.com>
2022-04-25 16:06:09 +0200

    test

--- Back/scriptEstablishment.php
+++ Back/scriptEstablishment.php
@@ -9,16 +9,16 @@ if(!isset($infoEstablishment)){
     $establishment = $infoEstablishment['establishment'];
 }
 
-$establishments = selectFromDatabase('establishment', 'id', $establishment, $conn);
+$establishment = selectFromDatabase('establishment', 'id', $establishment, $conn);
 // $sql = "SELECT* FROM establishment WHERE(id = $establishment)";
 // $result = mysqli_query($conn, $sql);
 // $establishments = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-foreach($establishments as $establishment){
+
     $establishmentID = $establishment['0'];
     session_start();
     $_SESSION['EstablishmentID'] = $establishmentID;
     header('Location: ../Front/PageEtablissement.php');
 
-}
+
 
