f0a029e1097347e71fbee5313ae2c557b15113c4
zozo222 <doerrzoran@gmail.com>
2022-04-25 07:57:59 +0200

    test

--- Front/temp.php
+++ Front/temp.php
@@ -1,11 +1,11 @@
 <?php
     require_once "../Back/ConnectToDatabase.php";
-    $query = "INSERT INTO user_hypnos Values('Test', 'Testant', test@test.mail', 'test', 3)";
+    $query = "INSERT INTO user_hypnos Values('Test', 'Testant', 'test@test.mail', 'test', 3)";
     $result = pg_query($conn, $query);
     if($reult){
         echo 'oui';
     }else{
-        echo 'non';
+        echo 
     }
 
 
