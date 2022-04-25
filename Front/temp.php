2b32b2a51b4a75c2e64bb8c7caa20e59f36c81dd
zozo222 <doerrzoran@gmail.com>
2022-04-25 02:19:12 +0200

    test

--- Front/temp.php
+++ Front/temp.php
@@ -2,7 +2,8 @@
     require_once "../Back/ConnectToDatabase.php";
     // function selectFromDatabase($table, $key, $data, $conn)
     // $resultquery = selectFromDatabase("user_hypnos", "firstname", "Doerr", $conn);
-    $resultquery = pg_insert("user_hypnos", ["name"=>"test", "firstname"=>"testant", "mail"=>"test@test.com", "password"=>"test" "role"=>3], $data, $conn);
+    $values =["name"=>"test", "firstname"=>"testant", "mail"=>"test@test.com", "password"=>"test" "role"=>3];
+    $resultquery = pg_insert("user_hypnos", $values, $data, $conn);
     if(!$resultquery)
     {
         echo("pas de résultats");
