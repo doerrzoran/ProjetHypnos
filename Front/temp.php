ae1f1691aa2069a94d2bf892e056d4f165a9d50d
zozo222 <doerrzoran@gmail.com>
2022-04-25 11:04:25 +0200

    test

--- Front/temp.php
+++ Front/temp.php
@@ -1,15 +1,4 @@
 <?php
     require_once "../Back/ConnectToDatabase.php";
-    echo 'test</br>';
-    // $query = pg_insert($conn, 'user_hypnos', ['test', 'testant', 'test@mail.com', 'test', 3]);
-    // $query = "INSERT INTO user_hypnos (name, firstname, mail, role, password) VALUES (test, testant, test@mail.com, test, 3)";
-    $name = 'test';
-    $firstname = 'testant';
-    $mail = 'test@mail.com';
-    $password = 'test';
-    $role = 3;
-    insertintoDatabase($table, $key, $data, $conn);
-    $test = insertintoDatabase('user_hypnos', 'name, firstname, mail, password, role', "'test', 'testant', 'test@mail.com', 'test', '3'", $conn);
-    // $query = "INSERT INTO user_hypnos (name, firstname, mail, password, role) VALUES ('test', 'testant', 'test@mail.com', 'test', '3')";
-    // "INSERT INTO book VALUES ('$name','$firstname', '$email','$role')";
-  print_r($test);
+            $verificationMail = selectFromDatabase('user_hypnos', '*', $mail, $conn);
+            var_dump($verificationMail);
