147b29d374b187eb7b1ee535df93e3aded4aff40
zozo222 <doerrzoran@gmail.com>
2022-04-25 11:30:02 +0200

    test

--- Front/temp.php
+++ Front/temp.php
@@ -1,10 +1,4 @@
 <?php
     require_once "../Back/ConnectToDatabase.php";
             $verificationMail = selectFromDatabase('user_hypnos', 'mail', 'abdelwoush@mail.com', $conn);
-            if($verificationMail){
-                echo 'commande valide</br>';
-                var_dump($verificationMail);
-                print_r($verificationMail);
-            }else{
-                'erreur</br>'
-            }
+            print_r ($verificationMail);
