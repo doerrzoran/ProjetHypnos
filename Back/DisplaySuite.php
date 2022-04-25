11979e6fa01b4bc1aed2d9374257ad4dd91a7b3b
zozo222 <doerrzoran@gmail.com>
2022-04-25 15:42:59 +0200

    test

--- Back/DisplaySuite.php
+++ Back/DisplaySuite.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'ConnectToDatabase.php';
 require_once 'UserID.php';
-
+echo '1';
 if(isset($_SESSION['role']) && ($_SESSION['role'] == 2)){
 
   $manager = $_SESSION['id'];
@@ -38,20 +38,21 @@ if(isset($_SESSION['role']) && ($_SESSION['role'] == 2)){
        echo $suitePrice.' €'.'</br>';
      }
 }else{
-
+  echo '2';
   $establishmentID = $_SESSION['EstablishmentID'];
 
 $establishments = selectFromDatabase('establishment', 'id', $establishmentID, $conn);
     $establishmentID = $establishment['0'];
     $establishmentName = $establishment['1'];
-
+    echo '3';
 
 echo '<h1>'.$establishmentName.'</h1>' ;
 
 $suites = selectAllFromDatabase('suite', 'establishment', $establishmentID, $conn);
-
+echo '4';
 
 foreach($suites as $suite){
+  echo '5';
     $suiteID = $suite['0'];
     $suiteTitle = $suite['1'];
     $suitePrice = $suite['3'];
@@ -62,7 +63,7 @@ foreach($suites as $suite){
     $suiteGallery = $suite['8'];
     $suiteGallery2 = $suite['9'];
     $suiteGallery3 = $suite['10'];
-
+  echo '6';
   echo '<div >
           <span >
              <h3>"'.$suiteTitle.'"</br></h3>
@@ -77,11 +78,13 @@ foreach($suites as $suite){
                <img class="d-inline" class="img-responsive"src="'.$suiteGallery3.'" width="100" height="100">
           </span></br>
         </div>';
-
+        echo '7';
     
      if($isOccupied == 1){
+      echo '8';
        echo "Cette chambre est reservée</br>";
      }else{
+      echo '9';
        echo "Disponible</br>";
        ?><form method="POST" action="../Back/scriptSuite.php" enctype="multipart/form-data">
          <input type="hidden" name="suite" value="<?= $suiteTitle ?>">
