db32ea551bf3291320de7beb0a46eed55054534b
zozo222 <doerrzoran@gmail.com>
2022-04-25 10:16:03 +0200

    test

--- Front/temp.php
+++ Front/temp.php
@@ -3,7 +3,12 @@
     echo 'test</br>';
     // $query = pg_insert($conn, 'user_hypnos', ['test', 'testant', 'test@mail.com', 'test', 3]);
     // $query = "INSERT INTO user_hypnos (name, firstname, mail, role, password) VALUES (test, testant, test@mail.com, test, 3)";
-    $query = "INSERT INTO user_hypnos (name, firstname, mail, password, role) VALUES (test, testant, test@mail.com, test, 3)";
+    $name = 'test';
+    $firstname = 'testant';
+    $mail = 'test@mail.com';
+    $password = 'test';
+    $role = 3;
+    $query = "INSERT INTO user_hypnos (name, firstname, mail, password, role) VALUES ('$name', '$firstname', '$mail', '$password', '$role')";
     // "INSERT INTO book VALUES ('$name','$firstname', '$email','$role')";
     if (pg_query($conn,$query))
 {
