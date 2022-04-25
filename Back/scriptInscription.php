14093b705a74665e278ef0b78cff7f881caac584
zozo222 <doerrzoran@gmail.com>
2022-04-25 11:02:35 +0200

    test

--- Back/scriptInscription.php
+++ Back/scriptInscription.php
@@ -8,23 +8,23 @@ if(!isset($infoCompte['name']) || !isset($infoCompte['firstname']) || !isset($in
   }else{ 
         $name = $infoCompte['name'];
         $firstname = $infoCompte['firstname'];
-        $mail = $infoCompte['email'];
+        $email = $infoCompte['email'];
         $password = $infoCompte['password'];
         $password2 = $infoCompte['Password2'];
       
         if($password != $password2){
             die("veuillez entrer un mot de passe identique!");
         }else{
-            echo $mail.'</br>';
+            echo $email.'</br>';
             require_once "ConnectToDatabase.php";
-            echo $mail.'</br>';
-            $verificationMail = selectFromDatabase('user_hypnos', '*', $mail, $conn);
+            echo $email.'</br>';
+            $verificationMail = selectFromDatabase('user_hypnos', '*', $email, $conn);
             var_dump($verificationMail);
             if($verificationMail != NULL){
                  exit('cette adresse email est déjà utilisée !');
             
             }else{ 
-                $result = insertintoDatabase( 'user_hypnos', 'name, firstname, mail, password, role', "'$name', '$firstname', '$mail', '$password', 3", $conn);
+                $result = insertintoDatabase( 'user_hypnos', 'name, firstname, email, password, role', "'$name', '$firstname', '$email', '$password', 3", $conn);
                 if($result != 1){
                     die("inscription échouée !");
                 }else{
