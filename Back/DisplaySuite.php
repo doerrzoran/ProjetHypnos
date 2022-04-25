27b806f7c0a5730afdb08ff8edf1846c18567045
zozo222 <doerrzoran@gmail.com>
2022-04-25 16:12:49 +0200

    test

--- Back/DisplaySuite.php
+++ Back/DisplaySuite.php
@@ -41,6 +41,7 @@ if(isset($_SESSION['role']) && ($_SESSION['role'] == 2)){
   echo '2';
   $establishmentID = $_SESSION['EstablishmentID'];
   echo $establishmentID;
+  
 
 $establishments = selectFromDatabase('establishment', 'id', $establishmentID, $conn);
     $establishmentID = $establishment['0'];
