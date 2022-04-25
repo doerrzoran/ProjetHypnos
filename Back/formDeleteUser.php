402cb14160d7c355a1bfb7f1273fd01cd244033a
zozo222 <doerrzoran@gmail.com>
2022-04-25 17:45:08 +0200

    test

--- Back/formDeleteUser.php
+++ Back/formDeleteUser.php
@@ -1,10 +1,8 @@
 <h2>supprimer un utilisateur</h2>
 <form method="POST" action="../Back/scriptDeleteUser.php" enctype="multipart/form-data">
-    <div class="form-group">
 	    <label>Identifiant:</label>
 	    <input type="text" name="mail">
 	    <label>Mot de passe:</label>
 	    <input type="text" name="password">
-	</div>
     <button type="submit">suppression du compte</button>
 </form>
