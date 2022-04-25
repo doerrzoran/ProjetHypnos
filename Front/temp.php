611b1b3008afe946b10595fbde206ab41673be80
zozo222 <doerrzoran@gmail.com>
2022-04-25 08:06:25 +0200

    test

--- Front/temp.php
+++ Front/temp.php
@@ -2,13 +2,13 @@
    echo 'lalallalalalal</br>';
     require_once "../Back/ConnectToDatabase.php";
     echo 'lalallalalalal</br>';
-    // $query = "INSERT INTO user_hypnos Values('Test', 'Testant', 'test@test.mail', 'test', 3)";
-    // $result = pg_query($conn, $query);
-    // if($reult){
-    //     echo 'oui';
-    // }else{
-    //     echo 
-    // }
+    $query = "INSERT INTO user_hypnos Values('Test', 'Testant', 'test@test.mail', 'test', 3)";
+    $result = pg_query($conn, $query);
+    if($reult){
+        echo 'oui';
+    }else{
+        echo 
+    }
 
 
     
