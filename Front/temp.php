365b4bd53503547606348920c63208046cf99236
zozo222 <doerrzoran@gmail.com>
2022-04-25 02:13:17 +0200

    test

--- Front/temp.php
+++ Front/temp.php
@@ -1,10 +1,16 @@
 <?php
     require_once "../Back/ConnectToDatabase.php";
     // function selectFromDatabase($table, $key, $data, $conn)
-    $resultquery = insertintoDatabase($table, $key, $data, $conn);
-    echo("Resultat ici :".'</br>');
+    // $resultquery = selectFromDatabase("user_hypnos", "firstname", "Doerr", $conn);
+    $resultquery = insertintoDatabase("user_hypnos", "[name=>test, firstname=>testant, mail=>test@test.com, password=>test role=>3]", $data, $conn)
+    if(!$resultquery)
+    {
+        echo("pas de résultats");
+    }
+    else {
+        print_r($resultquery);
+    }
+
+
     
-
-
-
 ?>
